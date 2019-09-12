72a89d5690c62530e3ed693525e6e27841d09463
Ermak1997 <toksikos123456@mail.ru>
2019-09-12 20:58:27 +0500

    Стиль страницы

---
+++ horor.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Ужасы!</h1>
+          <p><font size="5" color="black" face="Times New Roman">Данный сайт хранит фильмотеку жанра Ужасы, приятного простмотра!</front></p>
+          <p><a class="btn btn-secondary" href="index.php" role="button">назад »</a></p>
+                   
+        </div>
+      </div>
+</body>
+</html>
