23e26e93678553f956b302efc476f3a4a5d2f853
Ermak1997 <toksikos123456@mail.ru>
2019-09-12 21:40:03 +0500

    Закидываем стиль инфу о страницы

---
+++ all.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+ <!-- Main jumbotron for a primary marketing message or call to action -->
+ <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Галерея</h1>
+          <p>Данный сайт хранит фильмотеку всех жанров, приятного простмотра!</p>
+          <p><a class="btn btn-secondary" href="index.php" role="button">назад »</a></p>
+
+<div class="container">
+                  <!-- Example row of columns -->
+                  <div class="row">
+                    <div class="col-md-4">
+                    </div>
+</body>
+</html>
