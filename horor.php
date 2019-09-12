a2b5515dd7363c455b205352fd6dbfcd6d947523
Ermak1997 <toksikos123456@mail.ru>
2019-09-12 20:59:17 +0500

    Запрос к базе и сортировка по жанру

--- horor.php
+++ horor.php
@@ -15,7 +15,28 @@
           <h1 class="display-3">Ужасы!</h1>
           <p><font size="5" color="black" face="Times New Roman">Данный сайт хранит фильмотеку жанра Ужасы, приятного простмотра!</front></p>
           <p><a class="btn btn-secondary" href="index.php" role="button">назад »</a></p>
-                   
+          <?php
+        
+        $host = '127.0.0.1:3306';  
+        $user = 'root';    
+        $pass = '';
+        $db_name = 'basa';   
+        $link = mysqli_connect($host, $user, $pass, $db_name);
+      
+        if (!$link) {
+        echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
+        exit;
+      } 
+      if(!$link){
+        mysql_select_db($database_basa) or die(mysql_error());
+      }
+      $sql = mysqli_query($link, "select `films`.`name_f` from films where id_genre = 2");
+      While ($result=mysqli_fetch_array($sql))
+      Echo '<br>'.$result['name_f'].'</br>';
+      
+      ?>
+     
+    </p>     
         </div>
       </div>
 </body>
