3a73401f37ac06eacdf85c52b7a197bc41df484e
Ermak1997 <toksikos123456@mail.ru>
2019-09-12 20:51:04 +0500

    Создаем ссылку "назад" и заполняем информацию о странице

--- all.php
+++ all.php
@@ -9,5 +9,16 @@
     <title>Document</title>
 </head>
 <body>
+ <!-- Main jumbotron for a primary marketing message or call to action -->
+ <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Галерея</h1>
+          <p>Данный сайт хранит фильмотеку всех жанров, приятного простмотра!</p>
+          <p><a class="btn btn-secondary" href="index.php" role="button">назад »</a></p>
+
+<div class="container">
+                  <!-- Example row of columns -->
+                  <div class="row">
+                    <div class="col-md-4">
 </body>
 </html>
